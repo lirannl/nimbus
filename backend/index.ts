d681664a82578056491fb4f6b9dd1e3e7184126a
lirannl <liranpiade@gmail.com>
2020-10-24 20:40:11 +1000

    Fixed non-root routing

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
 import Env from 'dotenv';
 // Load production environment files
-Env.config({path: "/shared/nonsecret.env"});
-Env.config({path: "/shared/prod.env"});
-Env.config({path: `${__dirname}/../shared/dev.env`});
+Env.config({ path: "/shared/nonsecret.env" });
+Env.config({ path: "/shared/prod.env" });
+Env.config({ path: `${__dirname}/../shared/dev.env` });
 
 import Koa from 'koa';
 import Router from 'koa-router';
+import send from 'koa-send';
 import Serve from 'koa-static';
 import main from './routes/main';
 
@@ -23,8 +24,11 @@ api_r.use("/api", router.routes());
 if (process.env.TYPE == "development") App.use(require('@koa/cors')());
 
 App
-    .use(Serve("./res"))
+    .use(Serve("./res", { index: "index.html" }))
     .use(api_r.routes())
     ;
+
+// In production, if there's no other clear option, serve index.html
+if (process.env.TYPE != "development") App.use(async ctx => await send(ctx, "./res/index.html"));
 
 App.listen(parseInt(process.env.PORT || "3000"));
